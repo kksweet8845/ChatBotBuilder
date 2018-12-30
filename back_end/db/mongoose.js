dbf9b36b3e75f69897d36ec4a2ece2a65e58a162
kksweet8845 <s0913768710@gmail.com>
2018-12-30 18:22:26 +0000

    set up the environment

--- back_end/db/mongoose.js
+++ back_end/db/mongoose.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
-const config = require('./config.nober');
+const config = require('./config.chatBot');
 mongoose.Promise = global.Promise;
 mongoose.connect(`mongodb://${config.mongodb.user}:${config.mongodb.password}@${config.mongodb.host}/${config.mongodb.database}`,{
   useNewUrlParser: true
